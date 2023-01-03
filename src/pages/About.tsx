b6aa20b54026ecd4da6eb5a7837534dfc9d0179c
Evan McKinney <evmckinney9@gmail.com>
2023-01-02 21:22:27 -0500

    typing animation on about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,25 @@
-import React, { ReactElement, FC } from "react";
+import React, { ReactElement, FC, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
+import Typed from "typed.js";
 
 const About: FC<any> = (): ReactElement => {
+  useEffect(() => {
+    var options = {
+      strings: [
+        "I am a graduate student at the University of Pittsburgh.",
+        "I am a computer engineer, with a background in physics and math.",
+        "I am a programmer, fluent in Python, Java, and C.",
+      ],
+      typeSpeed: 60,
+      backSpeed: 45,
+      loop: true,
+      loopCount: Infinity,
+      smartBackspace: true
+    };
+
+    new Typed(".element", options);
+  }, []);
+
   return (
     <Box
       sx={{
@@ -12,9 +30,11 @@ const About: FC<any> = (): ReactElement => {
         alignItems: "center",
       }}
     >
-      <Typography variant="h3">About</Typography>
+      <Typography className="element" style={{ fontSize: "25px", fontFamily: "Consolas" }}></Typography>
+
     </Box>
   );
 };
 
 export default About;
+
